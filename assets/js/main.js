deec43bbbdcd756e4111e184a60367ab14b2d000
Anisha-Karmacharya <anisha.karmacharya.01@gmail.com>
2023-04-25 18:46:42 +0545

    - added js to hide and show dropdown menu - added style to the dropdown arrow

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,30 @@ const showMenu = (toggleId, navId) => {
     })
 }
 showMenu('nav-toggle', 'nav-menu')
+
+// DROPDOWN MENU
+const dropdownItems = document.querySelectorAll('.dropdown__item')
+dropdownItems.forEach((item) =>{
+    const dropdownButton = item.querySelector('.dropdown__button')
+    dropdownButton.addEventListener('click', () =>{
+        const showDropdown = document.querySelector('.show-dropdown')
+        toggleItem(item)
+        if(showDropdown && showDropdown != item){
+            toggleItem(showDropdown)
+        }
+    })
+})
+const toggleItem = (item) =>{
+    const dropdownContainer = item.querySelector('.dropdown__container')
+
+    if(item.classList.contains('show-dropdown')){
+        dropdownContainer.removeAttribute('style')
+        item.classList.remove('show-dropdown')
+    }
+    else{
+        dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
+        item.classList.add('show-dropdown')
+    }
+
+
+}
